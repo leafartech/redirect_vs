76fa417925b90c93cf53274ad8d57c33e21e392e
leafartech <119262053+leafartech@users.noreply.github.com>
2023-09-21 17:02:04 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,25 +34,25 @@ export default function Home() {
 
   const barStyle = {
     width: `${steps}%`,
-    transition: "width 0.5s ease-in-out" // Adicionando a animação ease-in-out
+    transition: "width 0.5s ease-in-out" // Adicionando a animação de ease-in-out
   };
 
   if (barStyle.width === '100%') {
     setTimeout(() => {
-      router.push('https://chat.whatsapp.com/JnXZQxZTcbDLvVW22UHdnI')
+      router.push('https://chat.whatsapp.com/L35opiIJZCMIYfAHoecivb')
     }, 500)
   }
 
   return (
-    <main className="w-full h-screen flex items-center justify-center bg-red-50">
-      <div className="relative flex flex-col text-center items-center justify-center h-[444px] px-12 rounded-md border border-gray-300">
+    <main className="w-full h-screen flex items-center justify-center bg-[#182934]/10">
+      <div className="relative flex flex-col text-center items-center justify-center h-[444px] px-12 rounded-md border border-[#182934]">
         <img src="./images/logo.png" alt="" className="-translate-y-6 w-16 h-16 rounded-full" />
         <div>
           <h2 className="text-3xl font-medium">Só um momento...</h2>
           <p>Estamos procurando o melhor grupo para você</p>
         </div>
         <div className="relative flex justify-center items-center rounded-md w-full h-6 mt-4 bg-gray-100 overflow-hidden">
-          <div className={`absolute left-0 h-full bg-[#df9ca0]`} style={barStyle}></div>
+          <div className={`absolute left-0 h-full bg-[#182934]`} style={barStyle}></div>
           {barStyle.width === '100%' && (
             <svg viewBox="25 25 50 50" className="z-10 absolute">
               <circle r="20" cy="50" cx="50"></circle>
